06436caea11b6e73910b38fdadb28cd5f02fcd27
Eze Stephen O <stonypeterz@outlook.com>
2018-12-09 12:54:48 +0100

    Updated UsersGroup Class

--- Freshly/Models/UsersPage.cs
+++ Freshly/Models/UsersPage.cs
@@ -8,16 +8,18 @@ namespace Freshly.Identity.Models {
         public List<UsersListItem> Items { get; set; }
         public int Number { get; set; }
         public int Size { get; set; }
+        public string FilterGroup { get; set; }
         public string FilterQ { get; set; }
         public int TotalPages { get; set; }
 
-        public UsersPage(List<UsersListItem> lst, int PNo, int PSize, int TotP, string fQ)
+        public UsersPage(List<UsersListItem> lst, int PNo, int PSize, int TotP, string fQ, string fG = "All")
         {
             Items = lst;
             Number = PNo;
             Size = PSize;
-            TotalPages = TotP;
+            TotalPages = TotP == 0 ? 1 : TotP;
             FilterQ = fQ;
+            FilterGroup = string.IsNullOrEmpty(fG) ? "All" : fG;
         }
     }
 
